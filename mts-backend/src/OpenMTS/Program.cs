66092d07f5d714fa2ffe3f303bfebacf137a3a04
Alexandre Charoy <alexandre.charoy@gmail.com>
2020-02-11 18:52:53 +0100

    VS project setup

---
+++ mts-backend/src/OpenMTS/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OpenMTS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
