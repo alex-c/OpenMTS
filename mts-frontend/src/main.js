46a2e1174d3ffdbcdce3b191b2dac8f596376cd0
Alexandre Charoy <alexandre.charoy@gmail.com>
2020-02-12 13:56:58 +0100

    Added i18n

--- mts-frontend/src/main.js
+++ mts-frontend/src/main.js
@@ -5,7 +5,36 @@ Vue.config.productionTip = false;
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/reset.css';
 import './theme/element-theme.scss';
+import enLocale from 'element-ui/lib/locale/lang/en';
+import deLocale from 'element-ui/lib/locale/lang/de';
 Vue.use(ElementUI);
+
+// Internationalization
+import VueI18n from 'vue-i18n';
+Vue.use(VueI18n);
+
+// Load messages
+const messages = {
+  en: {
+    login: {
+      header: 'User Login',
+    },
+    ...enLocale,
+  },
+  de: {
+    login: {
+      header: 'Anmeldung',
+    },
+    ...deLocale,
+  },
+};
+
+// Configure internationalization
+const i18n = new VueI18n({
+  locale: 'en',
+  fallbackLocale: 'en',
+  messages,
+});
 
 // Router & Vuex
 import router from './router';
@@ -16,5 +45,6 @@ import App from './App.vue';
 new Vue({
   router,
   store,
+  i18n,
   render: h => h(App),
 }).$mount('#app');
