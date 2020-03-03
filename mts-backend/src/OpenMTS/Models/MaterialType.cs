a244bfb7fa9bf875b9d248e88ff12eff17ea87d7
Alexandre Charoy <alexandre.charoy@gmail.com>
2020-03-03 19:43:27 +0100

    First draft of material-related models

---
+++ mts-backend/src/OpenMTS/Models/MaterialType.cs
@@ -0,0 +1,13 @@
+﻿namespace OpenMTS.Models
+{
+    /// <summary>
+    /// Represents a type of material.
+    /// </summary>
+    public enum MaterialType
+    {
+        /// <summary>
+        /// The material is a polypropylene.
+        /// </summary>
+        Polypropylene = 0
+    }
+}
---
+++ mts-backend/src/OpenMTS/Models/PropType.cs
@@ -0,0 +1,18 @@
+﻿namespace OpenMTS.Models
+{
+    /// <summary>
+    /// Specifices a data type for custom model properties.
+    /// </summary>
+    public enum PropType
+    {
+        /// <summary>
+        /// The property is a string.
+        /// </summary>
+        String = 0,
+
+        /// <summary>
+        /// The property is a file.
+        /// </summary>
+        File = 1
+    }
+}
