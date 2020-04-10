1f9daaeda50b079db7cae8df1bbbe2ee04e795d2
Alexandre Charoy <alexandre.charoy@gmail.com>
2020-04-10 19:59:29 +0200

    Set up the environment data service and reader

---
+++ mts-backend/src/OpenMTS/Services/Environment/EnvironmentalDataReader.cs
@@ -0,0 +1,124 @@
+﻿using Confluent.Kafka;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using OpenMTS.Models;
+using OpenMTS.Models.Environmnt;
+using System;
+using System.Collections.Concurrent;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace OpenMTS.Services.Environment
+{
+    /// <summary>
+    /// Reads data from Kafka for a given storage site.
+    /// </summary>
+    public class EnvironmentalDataReader
+    {
+        /// <summary>
+        /// The Kafka consumer configuration.
+        /// </summary>
+        private ConsumerConfig ConsumerConfig { get; }
+
+        /// <summary>
+        /// Thread-safe queue to write data to.
+        /// </summary>
+        private ConcurrentQueue<EnvironmentSnapshot> SnapshotQueue { get; }
+
+        /// <summary>
+        /// The ID of the storage site to read data for.
+        /// </summary>
+        private Guid StorageSiteId { get; }
+
+        /// <summary>
+        /// Kafka topic to read data from.
+        /// </summary>
+        private string Topic { get; }
+
+        /// <summary>
+        /// Used to log info.
+        /// </summary>
+        private ILogger Logger { get; }
+
+        /// <summary>
+        /// The worker task actually reading data from Kafka.
+        /// </summary>
+        private Task Worker { get; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="EnvironmentalDataReader"/> class.
+        /// </summary>
+        /// <param name="logger">Used to log information.</param>
+        /// <param name="snapshotQueue">Thread-safe queue to write data to.</param>
+        /// <param name="kafkaEndpoint">The Kafka endpoint to read data from.</param>
+        /// <param name="site">The storage site for which to read data.</param>
+        public EnvironmentalDataReader(ILogger<EnvironmentalDataReader> logger,
+            ConcurrentQueue<EnvironmentSnapshot> snapshotQueue,
+            string kafkaEndpoint,
+            StorageSite site)
+        {
+            StorageSiteId = site.Id;
+            SnapshotQueue = snapshotQueue;
+            ConsumerConfig = new ConsumerConfig
+            {
+                GroupId = "openmts-environment-consumers",
+                BootstrapServers = kafkaEndpoint,
+                AutoOffsetReset = AutoOffsetReset.Earliest
+            };
+            Topic = $"openmts-environment-{StorageSiteId}";
+            Logger = logger;
+
+            // Start consuming
+            Worker = StartConsuming();
+        }
+
+        /// <summary>
+        /// Starts and returns a task that reads environmental data from Kafka.
+        /// </summary>
+        /// <returns>Returns the new task.</returns>
+        private Task StartConsuming()
+        {
+            return new TaskFactory().StartNew(() =>
+            {
+                using (IConsumer<Ignore, string> consumer = new ConsumerBuilder<Ignore, string>(ConsumerConfig).Build())
+                {
+                    Logger.LogInformation($" + Starting to read environment data from topic `{Topic}`.");
+                    consumer.Subscribe(Topic);
+
+                    CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
+                    Console.CancelKeyPress += (_, @event) => {
+                        @event.Cancel = true;
+                        cancellationTokenSource.Cancel();
+                    };
+
+                    try
+                    {
+                        while (true)
+                        {
+                            try
+                            {
+                                ConsumeResult<Ignore, string> result = consumer.Consume(cancellationTokenSource.Token);
+                                EnvironmentSnapshot snapshot = JsonConvert.DeserializeObject<EnvironmentSnapshot>(result.Message.Value);
+                                snapshot.StorageSiteId = StorageSiteId;
+                                SnapshotQueue.Enqueue(snapshot);
+                                Logger.LogDebug($"Consumed message '{result.Message.Value}' at: '{result.TopicPartitionOffset}'.");
+                            }
+                            catch (ConsumeException exception)
+                            {
+                                Logger.LogError($"Error occured: {exception.Error.Reason}");
+                            }
+                        }
+                    }
+                    catch (OperationCanceledException)
+                    {
+                        consumer.Close();
+                    }
+                    finally
+                    {
+                        Logger.LogInformation($"Kafka consumer for topic `{Topic}` shut down.");
+                    }
+                }
+            }, TaskCreationOptions.LongRunning);
+        }
+    }
+}
---
+++ mts-backend/src/OpenMTS/Services/EnvironmentService.cs
@@ -0,0 +1,178 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using OpenMTS.Models;
+using OpenMTS.Models.Environmnt;
+using OpenMTS.Repositories;
+using OpenMTS.Services.Environment;
+using OpenMTS.Services.Support;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace OpenMTS.Services
+{
+    /// <summary>
+    /// A service for managing storage site environmental data.
+    /// </summary>
+    public class EnvironmentService : ISubscriber<StorageSite>
+    {
+        /// <summary>
+        /// A factory to create loggers from.
+        /// </summary>
+        private ILoggerFactory LoggerFactory { get; }
+
+        /// <summary>
+        /// Provides storage sites to manage environment data for.
+        /// </summary>
+        private LocationsService LocationsService { get; }
+
+        /// <summary>
+        /// Repository of environmental data.
+        /// </summary>
+        private IEnvironmentalDataRepository EnvironmentalDataRepository { get; }
+
+        /// <summary>
+        /// Data readers for each known storage site.
+        /// </summary>
+        private Dictionary<Guid, EnvironmentalDataReader> DataReaders { get; }
+
+        /// <summary>
+        /// A queue of environemtn snapshots to write to persist.
+        /// </summary>
+        private ConcurrentQueue<EnvironmentSnapshot> SnapshotQueue { get; }
+
+        /// <summary>
+        /// The task that consumes environmental snapshots from the queue to persist them.
+        /// </summary>
+        private Task QueueConsumer { get; }
+
+        /// <summary>
+        /// The Kafka endpoint.
+        /// </summary>
+        private string KafkaEndpoint { get; }
+
+        /// <summary>
+        /// A logger for local logging needs.
+        /// </summary>
+        private ILogger Logger { get; }
+
+        /// <summary>
+        /// Sets up the service with all necessare components. Gets all known storage sites and starts data readers for each of them.
+        /// </summary>
+        /// <param name="loggerFactory">A factory to create loggers from.</param>
+        /// <param name="configuration">The app configuration.</param>
+        /// <param name="locationsService">Provides storage sites.</param>
+        /// <param name="environmentalDataRepository">Repository of environmental data.</param>
+        public EnvironmentService(ILoggerFactory loggerFactory,
+            IConfiguration configuration,
+            LocationsService locationsService,
+            IEnvironmentalDataRepository environmentalDataRepository)
+        {
+            LoggerFactory = loggerFactory;
+            Logger = LoggerFactory.CreateLogger<EnvironmentService>();
+            LocationsService = locationsService;
+            EnvironmentalDataRepository = environmentalDataRepository;
+            SnapshotQueue = new ConcurrentQueue<EnvironmentSnapshot>();
+
+            // Get Kafka endpoint
+            KafkaEndpoint = configuration.GetValue("Kafka", "");
+            if (string.IsNullOrEmpty(KafkaEndpoint))
+            {
+                throw new Exception("Invalid Kafka endpoint provided!");
+            }
+
+            // Start reading from snapshot queue
+            QueueConsumer = new TaskFactory().StartNew(() =>
+            {
+                while (true)
+                {
+                    if (SnapshotQueue.TryDequeue(out EnvironmentSnapshot snapshot))
+                    {
+                        EnvironmentalDataRepository.RecordEnvironmentalSnapshot(snapshot);
+                    }
+                    else
+                    {
+                        Task.Delay(1000);
+                    }
+                }
+            });
+
+            // Set up data readers for all existing storage sites
+            DataReaders = new Dictionary<Guid, EnvironmentalDataReader>();
+            foreach (StorageSite site in LocationsService.GetAllStorageSites())
+            {
+                CreateAndRegisterDataReader(site);
+            }
+
+            // Subscribe to the creation of new storage sites
+            LocationsService.Subscribe(this);
+
+            // TODO: check for failed readers (Kafka not available?) and restart them
+        }
+
+
+        /// <summary>
+        /// Gets the latest value for a specific storage site and environmental factor.
+        /// </summary>
+        /// <param name="siteId">The ID of the storage site to get the latest value for.</param>
+        /// <param name="factor">The factor to get the latest value for.</param>
+        /// <returns>Returns the latest value, or null.</returns>
+        public DataPoint GetLatestValue(Guid siteId, EnvironmentalFactor factor)
+        {
+            StorageSite site = LocationsService.GetStorageSite(siteId);
+            return EnvironmentalDataRepository.GetLatestValue(site, factor);
+        }
+
+        /// <summary>
+        /// Gets the history of a specific storage site and environmental factor.
+        /// </summary>
+        /// <param name="siteId">The ID of the storage site to get the history for.</param>
+        /// <param name="factor">The factor to get the history for.</param>
+        /// <param name="startTime">Start time of the period to get value for.</param>
+        /// <param name="endTime">End time of the period to get value for.</param>
+        /// <returns>Returns the matching values.</returns>
+        public IEnumerable<DataPoint> GetHistory(Guid siteId, EnvironmentalFactor factor, DateTime startTime, DateTime endTime)
+        {
+            StorageSite site = LocationsService.GetStorageSite(siteId);
+            return EnvironmentalDataRepository.GetHistory(site, factor, startTime, endTime);
+        }
+
+        /// <summary>
+        /// Gets the min and max values for a specific storage site and environmental factor.
+        /// </summary>
+        /// <param name="siteId">The ID of the storage site to get the history for.</param>
+        /// <param name="factor">The factor to get the history for.</param>
+        /// <param name="startTime">Start time of the period to get value for.</param>
+        /// <param name="endTime">End time of the period to get value for.</param>
+        /// <returns>Returns the extrema.</returns>
+        public Extrema GetExtrema(Guid siteId, EnvironmentalFactor factor, DateTime startTime, DateTime endTime)
+        {
+            StorageSite site = LocationsService.GetStorageSite(siteId);
+            return EnvironmentalDataRepository.GetExtrema(site, factor, startTime, endTime);
+        }
+
+        /// <summary>
+        /// Fired when a new storage site has been created - creates a new environment data reader.
+        /// </summary>
+        /// <param name="site">New storage site.</param>
+        public void OnPublish(StorageSite site)
+        {
+            Logger.LogInformation("New site created - starting an environment data reader...");
+            CreateAndRegisterDataReader(site);
+        }
+
+        #region Private helpers
+
+        /// <summary>
+        /// Creates a new environment data reader, which immediately starts to read data from Kafka.
+        /// </summary>
+        /// <param name="site">The storage site the new reader is for.</param>
+        private void CreateAndRegisterDataReader(StorageSite site)
+        {
+            DataReaders.Add(site.Id, new EnvironmentalDataReader(LoggerFactory.CreateLogger<EnvironmentalDataReader>(), SnapshotQueue, KafkaEndpoint, site));
+        }
+
+        #endregion
+    }
+}
